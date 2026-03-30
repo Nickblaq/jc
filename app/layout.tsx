0189b3fadf92ed279af792a1c4bfaf829563a0ac
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2026-03-30 08:06:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 
 import type { Metadata } from 'next'
 import './globals.css'
+// import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500;600&family=DM+Mono:wght@400;500&display=swap');
+
+
 
 export const metadata: Metadata = {
   title: 'Short Editor',
