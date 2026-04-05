d6bbc0503c485263d0eb8a29e3796b15c0d75f62
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2026-04-05 01:04:59 +0100

    Update route.ts

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -34,9 +34,8 @@ export async function GET(request: NextRequest) {
     const stream = await yt.download(videoId as string, {
       quality: '360p',          // safe muxed stream
       type: 'video+audio',
-      client: 'IOS'
-      // format: 'mp4',
-      // codec: 'avc'              // iPhone-safe
+       format: 'mp4',
+      codec: 'avc'              // iPhone-safe
     })
 
     if (!stream) {
