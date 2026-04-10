924f7d0feb2406add81c91f8e686af215686330e
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2026-04-10 11:20:49 +0100

    Update route.ts

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,20 +2,39 @@
 import { NextResponse } from 'next/server'
 import { Innertube, UniversalCache } from 'youtubei.js'
 
+export const runtime = 'nodejs'
+export const maxDuration = 300
+
+// Singleton
+let _yt: Innertube | null = null
+
+async function getYT(): Promise<Innertube> {
+  if (!_yt) {
+    _yt = await Innertube.create({
+     // client_type: "ANDROID" as any,
+      cache: new UniversalCache(false),
+      generate_session_locally: true,
+    })
+  }
+  return _yt
+}
+
 export async function GET() {
   // Fetch static file from your own server
-  const res = await fetch('https://jc-ashy-kappa.vercel.app/ppg.mp4')
+  // 
+  const videoId = 'dQw4w9WgXcQ'
+ // const res = await fetch('https://jc-ashy-kappa.vercel.app/ppg.mp4')
 
-  if (!res.ok || !res.body) {
-    return NextResponse.json({ error: 'Failed to load test video' }, { status: 500 })
+  if (!videoId) {
+    return NextResponse.json({ error: 'Invalid Id' }, { status: 500 })
   }
 
-  return new NextResponse(res.body, {
-    headers: {
-      'Content-Type': 'video/mp4',
-      'Content-Length': res.headers.get('content-length') || '',
-      'Accept-Ranges': 'bytes',
-      'Cache-Control': 'no-store',
-    },
-  })
+  try {
+    const yt = await getYT()
+    const streams = await yt.download(videoId, {itag:18})
+  } catch {
+    
+  }
+
+  
 }
