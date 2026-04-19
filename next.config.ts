406b81edd751c864ca9e2ba99d1c42273d68e8f0
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2026-04-19 10:04:52 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    serverExternalPackages: ['youtubei.js'],
+  // env: {
+    // ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
+    //YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+  // },
 };
 
 export default nextConfig;
